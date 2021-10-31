618cbd82a4462342f75975c605370479cb9dc38f
Auraro <hediya116@gmail.com>
2021-10-31 14:40:14 +0800

    软件工程实验开发架构 version 1.0.0

--- src/main/java/top/ahcdc/periodical/interceptors/JWTInterceptor.java
+++ src/main/java/top/ahcdc/periodical/interceptors/JWTInterceptor.java
@@ -15,7 +15,7 @@ public class JWTInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         CommonResponse commonResponse;
-        String token = request.getHeader("token");//请求头中的token
+        String token = request.getHeader("authorization");//请求头中的token
 
         try{
             JWTUtils.verify(token);
